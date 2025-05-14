9147f3b48de92faa371845b0bba4aeee919c9638
Sriya Peddinti <sriya.peddinti@gmail.com>
2025-05-14 14:10:01 -0400

    project setup

---
+++ trailServer.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config({
+   path: path.resolve(__dirname, ".env"),
+});
+
+const app = express();
+const portNumber = 5000;
+const router1 = express.Router();
+
+
+/********* COMMAND LINE CODE *********/
+const server = app.listen(portNumber, () => {
+    console.log(`Web server is running at http://localhost:${portNumber}`);
+    process.stdout.write("Stop to shutdown the server: ");
+});
+
+process.stdin.on('readable', () => {
+	const dataInput = process.stdin.read();
+	if (dataInput !== null) {
+		const command = dataInput.trim();
+		if (command === "stop") {
+			process.stdout.write("Shutting down the server"); 
+            process.exit(0); 
+        }
+    }
+});
+
+
+/******* EXPRESS ROUTING CODE ********/
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "templates"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+router1.get('/', function(request, response) {
+    response.render("index.ejs");
+});
