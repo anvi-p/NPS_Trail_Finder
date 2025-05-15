61ca907080afd8bb6fdb6906524e0a8e0dee3cd9
Anvi Padiyar <anvipadiyar@gmail.com>
2025-05-15 01:37:15 -0400

    changed theme and added more pages

--- routes/hikes.js
+++ routes/hikes.js
@@ -12,20 +12,25 @@ router.get("/", (request, response) => {
 router.post("/getHikes", (request, response) => {
     const {state, difficulty} = request.body;
     const hikeCode = "BFF8C027-7C8F-480B-A5F8-CD8CE490BFBA";
-    const url = `https://developer.nps.gov/api/v1/thingstodo?q=${hikeCode}&&${difficulty}&stateCode=${state}&api_key=${process.env.API_KEY}&limit=20&sort=-relevanceScore`; 
+    const url = `https://developer.nps.gov/api/v1/thingstodo?stateCode=${state}&q=${hikeCode} AND ${difficulty}&api_key=${process.env.API_KEY}&limit=20&sort=-relevanceScore`; 
+    console.log(url);
     (async () => {
         const api_response = await fetch(url);
         const hikes = await api_response.json();
         const hikesList = hikes.data;
         let cardsList = "";
         hikesList.forEach(entry => {
+            let durationString = "";
+            if(entry.duration.length > 0){
+                durationString = `(${entry.duration})`;
+            }
             cardsList += `<article class="card">
-                            <h3>${entry.title}</h3>
+                            <h3>${entry.title} ${durationString}</h3>
                             <img src=${entry.images[0].url}>
-                            <h4><strong>${entry.duration}</strong></h4>
-                            <div class="description"><p>${entry.shortDescription}</p></div>
+                            <div class="description"><p>${entry.shortDescription}</p><div>
                             <footer><a href=${entry.url}>Learn more here!</a></footer>
-                        </article>`});
+                        </article>`;
+                    });
 
         response.render("retrieveHikes", {hikesCards: cardsList, state: state});
     })();
